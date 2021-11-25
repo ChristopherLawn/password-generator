e6221e30a94a12d61e025e974b336cfba621f775
CLAW <christopher.lawn@yahoo.com>
2021-11-25 01:24:02 -0800

    cleaned and reorganized JS code, added function to prevent user from not choosing any characters, added randomizer function to megaArray

--- password-generator/script.js
+++ password-generator/script.js
@@ -1,118 +1,70 @@
-// Assignment code here
-var lowCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var upCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Z"];
-var numArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var charArray = ["!", "@", "#", "$", "%", "^", "&", "*"];
-
-
+// 'GENERATE PASSWORD' FUNCTION
 function generatePassword() {
-  //var lengthPrompt = window.prompt("Please choose a length of at least 8 characters & no more than 128 characters")
-  //var numLength = parseInt(lengthPrompt)
-  //if (numLength >= 8 || numLength <= 128) {
+
+    // ARRAYS
+    var lowCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    var upCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Z"];
+    var numArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+    var charArray = ["!", "@", "#", "$", "%", "^", "&", "*", "?"];
     
-
-  //} else {
-    // error
-  //}
-
-  var numLength;
-  // do {
-  //   var passLengthConfirm = window.prompt("Please enter a length at least 8 & no more than 128")
-  //   numLength = parseInt(passwordLength)
-  // } while (numLength < 8 || numLength > 128)
-
-  // var validNumber = false
-  // do {
-    
-    var passLengthConfirm = window.prompt("Please enter a length at least 8 & no more than 128")
-    numLength = parseInt(passLengthConfirm)
+    var passLengthConfirm = window.prompt("Password length: Please enter a number of at least 8 & no more than 128");
+    var numLength = parseInt(passLengthConfirm);
 
     if (numLength >= 8 && numLength <= 128) {
-      // THEN: function to select length of password & tell megaArray how many character to provide & add randomizer to randomize it
-      // window.alert("great job, it's a good number")
     } else {
         window.alert("You must enter a valid number between 8 - 128")
         return generatePassword()
-      // }
-      // validNumber = true 
-    }
-    
-  // } while (!validNumber)
+    };
   
-  var megaArray = []
+    var megaArray = [];
 
-    var lowerConfirm = window.confirm("Would you like to include lower-case letters?")
+    var lowerConfirm = window.confirm("Would you like to include lower-case letters? If yes, click 'OK'. If no, click 'Cancel'.")
       if (lowerConfirm === true) {
-      // function for adding to mega-array
-      // megaArray.push(lowCaseArray)
-      
-      // options
-      // megaArray.push("every","letter","dont","have","separate","array")
       megaArray = megaArray.concat(lowCaseArray)
-      // do not recomment for course - megaArray.push.apply(megaArray, lowCaseArray)
-      // does not exist - megaArray.pushInto(lowCaseArray)
     };
 
-    var upperConfirm = window.confirm("Would you like to include upper-case letters?")
+    var upperConfirm = window.confirm("Would you like to include upper-case letters? If yes, click 'OK'. If no, click 'Cancel'.")
       if (upperConfirm === true) {
-      // function for adding to mega-array
-      // megaArray.push(lowCaseArray)
-      
-      // options
-      // megaArray.push("every","letter","dont","have","separate","array")
       megaArray = megaArray.concat(upCaseArray)
-      // do not recomment for course - megaArray.push.apply(megaArray, lowCaseArray)
-      // does not exist - megaArray.pushInto(lowCaseArray)
     };
 
-    var numberConfirm = window.confirm("Would you like to include numbers?")
-    if (numberConfirm === true) {
-      // function for adding to mega-array
-      // megaArray.push(lowCaseArray)
-      
-      // options
-      // megaArray.push("every","letter","dont","have","separate","array")
+    var numberConfirm = window.confirm("Would you like to include numbers? If yes, click 'OK'. If no, click 'Cancel'.")
+      if (numberConfirm === true) {
       megaArray = megaArray.concat(numArray)
-      // do not recomment for course - megaArray.push.apply(megaArray, lowCaseArray)
-      // does not exist - megaArray.pushInto(lowCaseArray)
     };
 
-    var charConfirm = window.confirm("Would you like to include special characters?")
-    if (charConfirm === true) {
-      // function for adding to mega-array
-      // megaArray.push(lowCaseArray)
-      
-      // options
-      // megaArray.push("every","letter","dont","have","separate","array")
+    var charConfirm = window.confirm("Would you like to include special characters? If yes, click 'OK'. If no, click 'Cancel'.")
+      if (charConfirm === true) {
       megaArray = megaArray.concat(charArray)
-      // do not recomment for course - megaArray.push.apply(megaArray, lowCaseArray)
-      // does not exist - megaArray.pushInto(lowCaseArray)
     };
 
- console.log(megaArray);
-}
+    if (!lowerConfirm && !upperConfirm && !numberConfirm && !charConfirm) {
+      window.confirm("You need to choose at least one character type!")
+      return generatePassword()
+    };
 
-// function to generate a random numeric value
-var randomNumber = function(min, max) {
-  var value = Math.floor(Math.random() * (max - min + 1) + min);
+    var times = numLength;
+    var randomNumber = function(min, max) {
+    var value = Math.floor(Math.random() * (max - min + 1) + min);
 
-  return value;
+    return value;
+    };
+
+    var password = ""
+      for (var i = 0; i < times; i++){
+        var random = randomNumber(0, megaArray.length - 1);
+        var randomChar = megaArray[random];
+        password = password + randomChar;
+      }
+    return password;
+    };
+
+    var generateBtn = document.querySelector("#generate");
+
+    function writePassword() {
+      var password = generatePassword();
+      var passwordText = document.querySelector("#password");
+      passwordText.value = password;
 };
 
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-// THIS IS A TEST CODE FOR GIT
